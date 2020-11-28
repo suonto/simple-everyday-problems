9f2e044b885b6a3339baf40b2d8dcfa21a57139a
Markus Suonto <markus.suonto@eficode.com>
2020-11-28 15:41:02 +0200

    Formatted data and main to support any language

--- json_serialization.py
+++ json_serialization.py
@@ -1,25 +1,19 @@
+import json
 import answer
 
-data = {
-    "toyota": {
-        "wheels": [
-            "big",
-            "small"
-        ],
-        "models": [
-            {
-                "corolla": {
-                    "color": {
-                        "light": "grey",
-                        "dark": "blue"
-                    }
-                }
-            }
-        ]
-    }
-}
+with open('cars.json', 'r') as f:
+    cars = json.load(f)
+
+with open('fruits.json', 'r') as f:
+    fruits = json.load(f)
 
 if __name__ == '__main__':
-    serialized = answer.serialize(data)
-    for k, v in serialized.items():
+    serialized_cars = answer.serialize(cars)
+    print('Serialized cars:')
+    for k, v in serialized_cars.items():
+        print(k, '=', v)
+    print('')
+    serialized_fruits = answer.serialize(fruits)
+    print('Serialized fruits:')
+    for k, v in serialized_fruits.items():
         print(k, '=', v)
